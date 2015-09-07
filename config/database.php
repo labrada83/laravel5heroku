5b8b563b46a7f6bfc1b1d0936358b96025673f6a
Amaury Labrada <labrada83@gmail.com>
2015-09-07 14:57:52 -0400

    DB configuration

--- config/database.php
+++ config/database.php
@@ -65,14 +65,14 @@ return [
         ],
 
         'pgsql' => [
-           /* 'driver'   => 'pgsql',
-            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
-            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-            'username' => parse_url(getenv("DATABASE_URL"))["user"],
-            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+            'driver'   => 'pgsql',
+            'host'     => env('DB_HOST', 'localhost'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset'  => 'utf8',
             'prefix'   => '',
-            'schema'   => 'public',*/
+            'schema'   => 'public',
         ],
 
         'sqlsrv' => [
--- resources/views/flyers/create.blade.php
+++ resources/views/flyers/create.blade.php
@@ -5,7 +5,7 @@
 
 	<h1>Selling your home?</h1>
 	<hr>
-	<form enctype="multipart/form-data" action="/flyers" method="POST" class="col-md-6">
+	<form enctype="multipart/form-data" action="/flyers" method="POST">
 		@include('flyers.form')
 
 		@if(count($errors) > 0)
